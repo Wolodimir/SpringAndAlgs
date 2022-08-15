110981b4cfa988a85d04f1d78cb969ef0dfc1bcb
Wolodimir <dvdent666@gmail.com>
2022-08-15 16:21:08 +0400

    Тесты

---
+++ src/test/java/com/learning/springandalgs/service/ArraysSortServiceTest.java
@@ -0,0 +1,21 @@
+package com.learning.springandalgs.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraysSortServiceTest {
+
+    public ArraysSortServiceTest(ArraysSortService arraysSortService) {
+    }
+
+    @Test
+    void bubbleSort() {
+    }
+
+    @Test
+    void selectionSort() {
+    }
+}

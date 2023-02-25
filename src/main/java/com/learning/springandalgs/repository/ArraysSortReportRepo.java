27511ab3f0a7b5659bf79bd32d30dddaca2b82f3
Wolodimir <dvdent666@gmail.com>
2023-02-25 12:09:15 +0400

    Change DB from Postgres to embedded H2

--- src/main/java/com/learning/springandalgs/repository/ArraysSortReportRepo.java
+++ src/main/java/com/learning/springandalgs/repository/ArraysSortReportRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArraysSortReportRepo extends JpaRepository<Long, ArraysSortReport> {
+public interface ArraysSortReportRepo extends JpaRepository<ArraysSortReport, Long> {
 }
